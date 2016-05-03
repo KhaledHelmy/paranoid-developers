c167e8219921d176433eba9670052eaae3787ed5
KhaledHelmy <khaled.a.helmy@gmail.com>
2016-05-03 23:45:34 +0200

    Modifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,15 @@ class SessionsController < Devise::SessionsController
 
   def create
     passphrase = params[:user][:passphrase]
-    params[:user].delete :passphrase
-    session[:passphrase] = passphrase
-    super
+    if passphrase.empty?
+      respond_to do |format|
+        format.html { redirect_to new_user_session_path, notice: 'Please enter passphrase!' }
+      end
+    else
+        params[:user].delete :passphrase
+        session[:passphrase] = passphrase
+        super
+    end
   end
 
 end
